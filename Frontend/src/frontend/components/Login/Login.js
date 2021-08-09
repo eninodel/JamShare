d1f3035334b36bb88577d18d499894f4fcf2d529
Edwin Nino Delgado <enino@uw.edu>
2021-08-08 22:04:33 -0700

    local host working edition

--- Frontend/src/frontend/components/Login/Login.js
+++ Frontend/src/frontend/components/Login/Login.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const AUTH_URL =
-  "https://accounts.spotify.com/authorize?client_id=dfe1eb532747437b9b7d84a113a3933f&response_type=code&redirect_uri=https://sharejams.herokuapp.com&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state";
+  "https://accounts.spotify.com/authorize?client_id=dfe1eb532747437b9b7d84a113a3933f&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state";
 
 function Login() {
   return (
